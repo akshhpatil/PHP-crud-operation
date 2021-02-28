c25d1c96bb24454b22e8890735de37e12b56d59c
Akshay Patil <76992034+akshhpatil@users.noreply.github.com>
2021-02-28 11:20:10 +0530

    Update update.php

--- update.php
+++ update.php
@@ -3,24 +3,19 @@
 <head>
 <title>update page </title>
 <link rel="stylesheet" type="text/css" href="style.css">
-
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 </head>
 <body class="body">
-
 <div class="container">
 <form id="form_group"  method="POST">
 
 <?php
 error_reporting (E_ALL ^ E_NOTICE);
-
 include 'connection.php';
-
 $id=$_GET['id'];
-
 if(isset($_POST['submit'])){
 header("location:dash.php");
 
@@ -34,7 +29,6 @@ $name=$_POST['name'];
 $email=$_POST['email'];
 $phone=$_POST['phone'];
 $address=$_POST['address'];
-
 
 $update = "UPDATE course SET id='$id',name='$name',email='$email',phone='$phone',address='$address' where id=$id";
 $query =mysqli_query($con,$update);
